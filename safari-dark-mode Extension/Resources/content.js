204ed5b7aeb5e8099e3d715961a4655d1433f139
pnegus <negus.patrickyi@gmail.com>
2025-08-20 23:31:18 -0400

    use text color heuristic

--- safari-dark-mode Extension/Resources/content.js
+++ safari-dark-mode Extension/Resources/content.js
@@ -5,20 +5,20 @@
 //  Created by Patrick Negus on 8/20/25.
 //
 
-function isColorLight(colorString) {
+function isTextDark(colorString) {
     const rgb = colorString.match(/\d+/g);
     if (!rgb) {
         return true;
     }
     const luminance = (0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]) / 255;
-    return luminance > 0.2;
+    return luminance < 0.2;
 }
 
 document.documentElement.classList.add('sdm_dark_mode_preload');
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    const backgroundColor = window.getComputedStyle(document.body).backgroundColor;
-    if (isColorLight(backgroundColor)) {
+    const textColor = window.getComputedStyle(document.body).color;
+    if (isTextDark(textColor)) {
         document.documentElement.classList.add('sdm_dark_mode');
     }
     document.documentElement.classList.remove('sdm_dark_mode_preload');
