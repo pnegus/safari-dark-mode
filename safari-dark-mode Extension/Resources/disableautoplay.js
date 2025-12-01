e62160115c1a69b428d42e4b808b087f349700e8
pnegus <negus.patrickyi@gmail.com>
2025-12-01 13:55:00 -0500

    improve logic

--- safari-dark-mode Extension/Resources/disableautoplay.js
+++ safari-dark-mode Extension/Resources/disableautoplay.js
@@ -1,25 +1,29 @@
 const codeToInject = `
 (function() {
+    if (window.__autoplayBlocked) return;
+    window.__autoplayBlocked = true;
+
     const originalPlay = HTMLMediaElement.prototype.play;
 
     HTMLMediaElement.prototype.play = function() {
-        const isUserInteraction = window.event && (
-            window.event.type === 'click' || 
-            window.event.type === 'keydown' ||
-            window.event.type === 'mousedown' ||
-            window.event.type === 'touchstart'
+        const hasModernActivation = navigator.userActivation && navigator.userActivation.isActive;
+        const event = window.event;
+        const hasLegacyActivation = event && event.isTrusted && (
+            event.type === 'click' || 
+            event.type === 'keydown' || 
+            event.type === 'mousedown' || 
+            event.type === 'touchstart' ||
+            event.type === 'pointerdown'
         );
+
+        const isUserInteraction = hasModernActivation || hasLegacyActivation;
 
         if (isUserInteraction) {
             return originalPlay.apply(this, arguments);
         } else {
-            return originalPlay.apply(this, arguments)
-                .then(() => {
-                    this.pause();
-                    console.log('shadowdom autoplay blocked');
-                })
-                .catch(err => {
-                });
+            console.warn('ShadowDOM Autoplay blocked by Safari Extension');
+            
+            return Promise.reject(new DOMException('The play() request was interrupted by an extension.', 'NotAllowedError'));
         }
     };
 })();
@@ -27,6 +31,11 @@ const codeToInject = `
 
 const script = document.createElement('script');
 script.textContent = codeToInject;
+
+const existingScriptWithNonce = document.querySelector('script[nonce]');
+if (existingScriptWithNonce) {
+    script.setAttribute('nonce', existingScriptWithNonce.nonce || existingScriptWithNonce.getAttribute('nonce'));
+}
 
 (document.head || document.documentElement).appendChild(script);
 
